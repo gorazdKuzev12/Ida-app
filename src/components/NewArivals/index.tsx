c4c8557f8dc295cb7e314b0b9ad89680d432a7cf
gorazdKuzev12 <gorazd.kuzev10@gmail.com>
2023-10-02 13:41:15 +0200

    feat: Added new images

--- src/components/NewArivals/index.tsx
+++ src/components/NewArivals/index.tsx
@@ -5,70 +5,75 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   height: 140vh;
-  padding: 20px;
+  padding: 10px;
 `;
 
 const LeftPanel = styled.div`
+  position: relative;
   flex: 50%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
+  cursor: pointer;
 `;
 
 const RightPanel = styled.div`
+  position: relative;
   flex: 50%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const ImageWrapper = styled.div`
+  width: 72%;
+  height: 70%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Image = styled.img`
-  width: 70%;
-  height: auto;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
-const LeftImage = styled(Image)`
-  max-height: 70%;
-`;
-
-const RightImage = styled(Image)`
-  max-height: 55%;
-`;
-
-const DiscountText = styled.div`
-  margin-top: 10px;
-  font-size: 1.5em;
+const OverlayText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
   font-weight: bold;
-  color: gray;
-`;
-
-const ShopNow = styled.a`
-  margin-top: 10px;
-  cursor: pointer;
-  text-decoration: underline;
-  color: gray;
-`;
-
-const NewArrivalsText = styled.div`
-  margin-top: 10px;
-  font-size: 1.5em;
-  font-weight: bold;
-  color: gray;
+  font-size: 2em;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
 const NewArrivals = () => {
   return (
     <Container>
       <LeftPanel>
-        <LeftImage src="/images/kids4.jpg" alt="Left Product Image" />
-        <DiscountText>25% Off all Jeans</DiscountText>
-        <ShopNow>Shop now</ShopNow>
+        <ImageWrapper>
+          <Image src="/images/women.jpg" alt="Left Product Image" />
+        </ImageWrapper>
+        <OverlayText>WOMEN</OverlayText>
       </LeftPanel>
       <RightPanel>
-        <RightImage src="/images/kids5.jpg" alt="Right Product Image" />
-        <NewArrivalsText>Introducing out new colection</NewArrivalsText>
+        <ImageWrapper>
+          <Image src="/images/man.jpg" alt="Right Product Image" />
+        </ImageWrapper>
+        <OverlayText>MAN</OverlayText>
       </RightPanel>
     </Container>
   );
